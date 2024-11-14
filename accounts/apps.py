0b2cdac205ddfe30683549c9355723bf89e99d51
AlbertJin1 <awsomegamingracerhd3@gmail.com>
2024-11-14 21:24:06 +0800

    "Updated various Python files, migrations, and database backups; removed some database backup files."

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,7 +1,4 @@
 from django.apps import AppConfig
-from django.core.management import call_command
-from django.db import connections
-from django.db.utils import OperationalError
 
 
 class AccountsConfig(AppConfig):
@@ -12,19 +9,4 @@ class AccountsConfig(AppConfig):
         # Import the signals module
         import accounts.signals
 
-        # Try to run makemigrations and then migrate after the server starts
-        try:
-            # Check if the database is ready
-            connection = connections["default"]
-            connection.cursor()
-
-            # Run makemigrations first
-            call_command(
-                "makemigrations", "accounts"
-            )  # This will create migrations if any changes exist
-            # Then, run migrations
-            call_command("migrate")  # Apply migrations
-
-        except OperationalError:
-            # Handle the case where the database is not ready yet
-            print("Database is not ready, retrying migration later.")
+        # No database access or migrations here
